1099ba116f30b7d5b9dfc962d6ecef7275a6d82b
Sara Markowitz <sara.markowitz@mrjaffesclass.com>
2023-09-11 11:38:55 -0700

    almost done

---
+++ Operater.java
@@ -0,0 +1,13 @@
+
+
+public class Operater
+{
+    public static void main(String[] args) {
+        int i=10;
+        int f=i+f;
+        boolean flag = true;
+        i++;
+        System.out.println("value of i");
+        System.out.println("value of f");
+    }
+}
